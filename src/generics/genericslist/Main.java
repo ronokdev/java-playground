50af9860cbb9822c846678a6151b50ad27531fcb
farhan.fuad <farhan.fuad.synesisit@gmail.com>
2021-02-17 18:06:10 +0600

    fix(generic): comment add to generic class

--- src/generics/genericslist/Main.java
+++ src/generics/genericslist/Main.java
@@ -3,7 +3,7 @@ package generics.genericslist;
 public class Main {
     public static void main(String[] args)
     {
-        GenericList genericList = new GenericList<Integer>();
+        GenericList genericList = new GenericList<Integer>();  // we can only pass reference types we can not pass primitive types
         genericList.add(1);
         System.out.println(genericList.get(0));
     }
