35e6e64cf4651f25612ab3ee2f67a775d3fc59dc
Ronok <fuad.ronok@gmail.com>
2021-03-10 21:54:58 +0600

    fix(refactoring): some refactoring done on InterfaceSegregation class

--- src/com/interfacesegregation/InterfaceSegregation.java
+++ src/com/interfacesegregation/InterfaceSegregation.java
@@ -4,10 +4,12 @@ public class InterfaceSegregation
 {
     public static void main(String[] args)
     {
-        TextBox textBox = new TextBox();
-        textBox.drag();
-
-        DropDownMenu dropDownMenu = new DropDownMenu();
-        dropDownMenu.drag();
+        dragSomething(new TextBox());
+        dragSomething(new DropDownMenu());
     }
+
+    public static void dragSomething(UiWidget uiWidget){
+       uiWidget.drag();
+    }
+
 }
