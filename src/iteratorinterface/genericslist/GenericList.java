8990fe84324a950aab53ed0194f705bd275b845e
Ronok <fuad.ronok@gmail.com>
2021-03-24 21:16:14 +0600

    feat(iterator-interface): Iterator interface implimented

---
+++ src/iteratorinterface/genericslist/GenericList.java
@@ -0,0 +1,43 @@
+package iteratorinterface.genericslist;
+
+public class GenericList<T> implements Iterable<T> {
+    private T[] items = (T[])new Object[10];
+    private int count;
+
+    public void add(T item){
+       items[count++] = item;
+    }
+
+    public T get(int index){
+        return items[index];
+    }
+
+    @Override
+    public ListIterator<T> iterator()
+    {
+        return new ListIterator(this);
+    }
+
+
+    private class ListIterator<T> implements java.util.Iterator{
+
+        private final GenericList<T> fullGenericList;
+        private int index;
+        public ListIterator(GenericList<T> genericList)
+        {
+           fullGenericList = genericList;
+        }
+
+        @Override
+        public boolean hasNext()
+        {
+            return (index<fullGenericList.count);
+        }
+
+        @Override
+        public T next()
+        {
+            return fullGenericList.items[index++];
+        }
+    }
+}
