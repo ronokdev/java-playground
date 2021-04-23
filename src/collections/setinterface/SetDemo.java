8e0c4d26a3a887ca48e4970c2eda7982e5dda460
farhan.fuad <farhan.fuad.synesisit@gmail.com>
2021-04-23 17:07:13 +0600

    feat(set-interface): a class implemented with different types of Set Operations

--- src/collections/setinterface/SetDemo.java
+++ src/collections/setinterface/SetDemo.java
@@ -23,5 +23,28 @@ public class SetDemo {
         Set<String> stringSetNew = new HashSet<>(stringCollection);
         System.out.println(stringSetNew);
 
+        //pass a list of string to a set
+        Set<String> stringSet1 = new HashSet<>(Arrays.asList("d","e","f"));
+
+
+        Set<String> stringSet2 = new HashSet<>(Arrays.asList("e","f","g"));
+        System.out.println();
+        System.out.println("This is string set 1 :"+stringSet1);
+        System.out.println("This is string set 2 :"+stringSet2);
+
+        //Different types of Set Operation
+        //As this operations effect the OBJECT of a SET that's why we are commenting in every operation EXCEPT the one we want to see the results.
+
+        //Union
+        stringSet1.addAll(stringSet2); // output is: [d, e, f, g]
+        System.out.println("After Union :"+stringSet1);
+
+        //Intersection
+        //stringSet1.retainAll(stringSet2);
+        //System.out.println("After Intersection :" +stringSet1); //output: [e,f]
+
+        //Difference
+        //stringSet1.removeAll(stringSet2);
+        //System.out.println(stringSet1); //Output: [d] because it's keeping everything in stringSet1 and removing everything from stringSet2
     }
 }
