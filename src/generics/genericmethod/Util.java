aff33ca92df52598372173eb07822c683ca5b855
Ronok <fuad.ronok@gmail.com>
2021-04-25 02:39:20 +0600

    fix(comment): comment add for genericmethod implementation

--- src/generics/genericmethod/Util.java
+++ src/generics/genericmethod/Util.java
@@ -3,6 +3,7 @@ package generics.genericmethod;
 public class Util {
     public <T extends Comparable<T>> T checkBalance(T first, T second)
     {
+        //first.compareTo(second) returns POSITIVE integer if it's True, Otherwise it returns a negative integer
         return (first.compareTo(second)>1)?first:second;
     }
 
