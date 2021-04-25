12ba00b0e7b565e565ae6cfb54c93693a0983c10
farhan.fuad <farhan.fuad.synesisit@gmail.com>
2021-04-26 01:58:49 +0600

    feat(map-interface): a class implemented with different types of Hashmap Operations with Map Interface

---
+++ src/collections/mapinterface/Main.java
@@ -0,0 +1,7 @@
+package collections.mapinterface;
+
+public class Main {
+    public static void main(String[] args){
+        MapDemo.showMapDemo();
+    }
+}
---
+++ src/collections/mapinterface/MapDemo.java
@@ -0,0 +1,53 @@
+package collections.mapinterface;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MapDemo {
+    public static void showMapDemo(){
+
+        Customer customer1 = new Customer("Ronok","ronok@gmail.com");
+        Customer customer2 = new Customer("Ratul","ratul@gmail.com");
+        Customer customer3 = new Customer("Koushika","koushika@gmail.com");
+        Customer unknownCustomer = new Customer("Unknown","No Email");
+
+        Map<String,Customer> customerMap = new HashMap<>();
+
+        //Using email as Key, adding object to HashMap.
+        customerMap.put(customer1.getName(),customer1);
+        customerMap.put(customer3.getName(),customer3);
+        customerMap.put(customer2.getName(),customer2);
+
+
+        //Get an object from Hashmap
+        System.out.println(customerMap.get("Ratul"));
+        //Setting a default object in-case of Not finding the object in the HashMap
+        System.out.println(customerMap.getOrDefault("ronok@gmail",unknownCustomer));
+
+        //Checking if a key Exists
+        System.out.println(customerMap.containsKey("ronok@gmail.com"));
+        System.out.println(customerMap.containsKey("Ronok"));
+
+        //Replacing an Existing Object int the HashMap
+        customerMap.replace("Ratul",new Customer("Ratul","Ratul@yahoo.com"));
+        System.out.println("Printing the Full HashMap: "+customerMap);
+
+        //Iterate over a hashMap
+
+        //Getting all the Objects from hashmap by iterating over the Keys
+        for(String key :customerMap.keySet()){
+            System.out.println(customerMap.get(key));
+        }
+
+        //Iterating with EntrySet
+        for(Map.Entry<String, Customer> key :customerMap.entrySet()){
+            System.out.println((key.getValue()));
+        }
+
+        //Iterating with values which returns one object with each Iteration
+        for(Customer customers : customerMap.values()){
+            System.out.println(customers);
+        }
+    }
+}
+
