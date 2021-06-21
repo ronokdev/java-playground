64fdb3bb535cac46a4ebf27394fdbca7bdf52415
farhan.fuad <farhan.fuad.synesisit@gmail.com>
2021-06-22 00:39:39 +0600

    feat(multiple-thread): start and end Multiple thread with example

---
+++ src/threadandconcurrency/multiplethreadstart/MultipleThreadStartDemo.java
@@ -0,0 +1,16 @@
+package threadandconcurrency.multiplethreadstart;
+
+public class MultipleThreadStartDemo {
+
+    public static void main(String[] args){
+
+        //Printing the MAIN Thread information
+        System.out.println("Main Thread ID :: "+Thread.currentThread().getId()+" Name :: "+Thread.currentThread().getName());
+
+        for(int i=0;i<10;i++){
+            Thread thread = new Thread(new FileDownload());
+            thread.start();
+        }
+
+    }
+}
