0cc5fb2d4dcee49a075ec1358263c17784e7c6e4
farhan.fuad <farhan.fuad.synesisit@gmail.com>
2021-07-02 22:57:51 +0600

    feat(RaceCondition Solution-Confinement): Confinement of object implemeted with example

---
+++ src/threadandconcurrency/raceconditionsolutions/confinement/DownloadStatus.java
@@ -0,0 +1,13 @@
+package threadandconcurrency.raceconditionsolutions.confinement;
+
+public class DownloadStatus {
+    private int totalBytes;
+
+    public int getTotalBytes() {
+        return totalBytes;
+    }
+
+    public void incrementTotalBites(){
+        totalBytes++;
+    }
+}
