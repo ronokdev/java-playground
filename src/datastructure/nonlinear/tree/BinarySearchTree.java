9e8102a0ee7736477027bbc0927f5de66769bed3
farhan.fuad <farhan.fuad.synesisit@gmail.com>
2021-09-10 20:48:38 +0600

    fix (binarySearch Tree) :
    - static class introduced inside BinarySearchTree Class
    - New Tree value added
    - using of 'this' keyword started

--- src/datastructure/nonlinear/tree/BinarySearchTree.java
+++ src/datastructure/nonlinear/tree/BinarySearchTree.java
@@ -8,122 +8,124 @@ public class BinarySearchTree {
     * the Node class should have 3 fields
     * value, reference of the left child, reference of the right child
     * */
-    private class Node{
+
+    /*
+    * we can not use Static in the Outer Class
+    * So, we are using static in the Inner class.
+    * */
+    static class Node {
         private int value;
         private Node leftChild;
         private Node rightChild;
 
         @Override
         public String toString() {
-            return "Node= " +value ;
+            return "Node= " + value;
         }
 
-        public Node(int value) {
-            this.value = value;
-        }
-    }
-
-    private Node root;
-
-    /*
-    * For inserting a new value into a binary search tree
-    * if the inserting value is less than the value of the current Node then we will insert the value as a left child
-    * if the inserting value is greater than the value of the current Node then we will insert the value as a right child
-    * */
-    public void insert(int value){
-        if(root == null){
-            root = new Node(value);
-            return;
+        public Node(int rootValue) {
+            this.value = rootValue;
         }
 
-        Node current = root;
-        while(true){
-            if(value < current.value) {
-                if(current.leftChild == null){
-                    current.leftChild = new Node(value);
-                    break;
+
+        /*
+         * For inserting a new value into a binary search tree
+         * if the inserting value is less than the value of the current Node then we will insert the value as a left child
+         * if the inserting value is greater than the value of the current Node then we will insert the value as a right child
+         * */
+        public void insert(int value) {
+        Node current = this;
+            while (true) {
+                if (value < current.value) {
+                    if (current.leftChild == null) {
+                        current.leftChild = new Node(value);
+                        break;
+                    }
+                    current = current.leftChild;
+                } else {
+                    if (current.rightChild == null) {
+                        current.rightChild = new Node(value);
+                        break;
+                    }
+                    current = current.rightChild;
                 }
-                current = current.leftChild;
-            }
-            else{
-                if(current.rightChild== null){
-                current.rightChild= new Node(value);
-                break;
-            }
-                current = current.rightChild;
             }
         }
-    }
 
-    /*
-    * For finding a value in a binary search tree
-    * If the finding value is less than the value of current node we will look in the left child of the tree
-    * If the finding value is greater than the value of current node we will look in the right child of the tree
-    * */
-    public String find(int value){
-        Node current = root;
+        /*
+         * For finding a value in a binary search tree
+         * If the finding value is less than the value of current node we will look in the left child of the tree
+         * If the finding value is greater than the value of current node we will look in the right child of the tree
+         * */
+        public void find(int value) {
+            Node current = this;
 
-        while(current != null){
-           if(value < current.value)
-               current = current.leftChild;
-           else if(value > current.value)
-               current = current.rightChild;
-           else
-               return value+" found";
+            while (current != null) {
+                if (value < current.value)
+                    current = current.leftChild;
+                else if (value > current.value)
+                    current = current.rightChild;
+                else{
+                    System.out.println(value + " found");
+                    return;
+                }
+            }
+            System.out.println(value + " NOT found");
         }
-        return value+" NOT found";
+
+        /*
+         * For doing preOrderTraversal we will do
+         * root, leftChild, rightChild
+         * */
+        public void preOrderTraversal() {
+            preOrderTraversal(this);
+        }
+
+        private void preOrderTraversal(Node rootNode) {
+            if (rootNode == null)
+                return;
+
+            System.out.println(rootNode.value);
+            preOrderTraversal(rootNode.leftChild);
+            preOrderTraversal(rootNode.rightChild);
+        }
+
+
+        /*
+         * For doing inOrderTraversal we will do
+         * leftChild, root, rightChild
+         * */
+        public void inOrderTraversal() {
+            inOrderTraversal(this);
+        }
+
+        private void inOrderTraversal(Node rootNode) {
+            if (rootNode == null)
+                return;
+
+            inOrderTraversal(rootNode.leftChild);
+            System.out.println(rootNode.value);
+            inOrderTraversal(rootNode.rightChild);
+        }
+
+
+        /*
+         * For doing postOrderTraversal we will do
+         * leftChild, rightChild, root
+         * */
+        public void postOrderTraversal() {
+            postOrderTraversal(this);
+        }
+
+        private void postOrderTraversal(Node rootNode) {
+            if (rootNode == null)
+                return;
+
+            postOrderTraversal(rootNode.leftChild);
+            postOrderTraversal(rootNode.rightChild);
+            System.out.println(rootNode.value);
+        }
+
+
     }
-
-    /*
-    * For doing preOrderTraversal we will do
-    * root, leftChild, rightChild
-    * */
-    public void preOrderTraversal(){
-        preOrderTraversal(root);
-    }
-    private void preOrderTraversal(Node rootNode){
-        if(rootNode == null)
-            return;
-
-        System.out.println(rootNode.value);
-        preOrderTraversal(rootNode.leftChild);
-        preOrderTraversal(rootNode.rightChild);
-    }
-
-
-    /*
-     * For doing inOrderTraversal we will do
-     * leftChild, root, rightChild
-     * */
-    public void inOrderTraversal(){
-        inOrderTraversal(root);
-    }
-    private void inOrderTraversal(Node rootNode){
-        if(rootNode == null)
-            return;
-
-        inOrderTraversal(rootNode.leftChild);
-        System.out.println(rootNode.value);
-        inOrderTraversal(rootNode.rightChild);
-    }
-
-
-
-    /*
-     * For doing postOrderTraversal we will do
-     * leftChild, rightChild, root
-     * */
-    public void postOrderTraversal(){
-        postOrderTraversal(root);
-    }
-    private void postOrderTraversal(Node rootNode){
-        if(rootNode == null)
-            return;
-
-        postOrderTraversal(rootNode.leftChild);
-        postOrderTraversal(rootNode.rightChild);
-        System.out.println(rootNode.value);
-    }
-
-
 }
--- src/datastructure/nonlinear/tree/Main.java
+++ src/datastructure/nonlinear/tree/Main.java
@@ -2,17 +2,23 @@ package datastructure.nonlinear.tree;
 
 public class Main {
     public static void main(String[] args){
-        BinarySearchTree binarySearchTree = new BinarySearchTree();
-        binarySearchTree.insert(7);
-        binarySearchTree.insert(4);
-        binarySearchTree.insert(9);
-        binarySearchTree.insert(1);
-        binarySearchTree.insert(6);
-        binarySearchTree.insert(8);
-        binarySearchTree.insert(10);
+        BinarySearchTree.Node binarySearchTree = new BinarySearchTree.Node(6);
 
-        System.out.println(binarySearchTree.find(10));
-        System.out.println(binarySearchTree.find(11));
+        binarySearchTree.insert(4);
+        binarySearchTree.insert(14);
+        binarySearchTree.insert(1);
+        binarySearchTree.insert(5);
+        binarySearchTree.insert(8);
+        binarySearchTree.insert(17);
+        binarySearchTree.insert(9);
+        binarySearchTree.insert(7);
+        binarySearchTree.insert(16);
+        binarySearchTree.insert(18);
+
+        binarySearchTree.find(10);
+        binarySearchTree.find(11);
+        binarySearchTree.find(9);
+        binarySearchTree.find(6);
 
         System.out.println("Pre Order");
         binarySearchTree.preOrderTraversal();
