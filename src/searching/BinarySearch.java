e260916ce210cab95200bae2df8bdab9bee4fd99
farhan.fuad <farhan.fuad.synesisit@gmail.com>
2021-10-18 00:04:13 +0600

    feat(binary search): start write binary Search algo

---
+++ src/searching/BinarySearch.java
@@ -0,0 +1,10 @@
+package searching;
+
+public class BinarySearch {
+    public static void main (String[] args){
+        System.out.println("Binary Search In Action");
+        /*
+        * Need to Write the Loop and Iterative approach
+        * */
+    }
+}
